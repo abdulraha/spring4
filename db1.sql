696480c1e8406c2167bb5dc0c8f13fc83530b832
Abdulr <laddu035@gmail.com>
2017-11-29 13:54:09 +0530

    Initial project setup

---
+++ db1.sql
@@ -0,0 +1,8 @@
+create database happiestminds;
+create table Employees (
+        id bigint not null auto_increment,
+        age integer,
+        name varchar(255),
+        salary float,
+        primary key (id)
+);
